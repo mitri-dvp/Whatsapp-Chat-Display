2de58fa31116593fdef110a487fc4b6bdf9ce68f
mitri-dvp <mitri.dvp@gmail.com>
2021-07-23 17:47:58 -0300

    View all functionality added in both messages and activity

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const introductionDOM = document.querySelector('.introduction')
 let _i = 0
 let prevUser = -1;
 let prevDate = -1;
-let viewMoreDOM;
+let buttonsContainerDOM;
 
 
 // Listeners
@@ -114,16 +114,34 @@ function populateMainView(result) {
   for (let i = 0; i < result.messages.length; i++) { 
     _i++
     if(_i > 50) {
+      const newButtonsContainerDOM = document.createElement('div')
+      newButtonsContainerDOM.classList.add('buttons')
+
       const newViewMoreDOM = document.createElement('div')
       newViewMoreDOM.classList.add('view_more')
       newViewMoreDOM.innerHTML = `ver más`
+      newButtonsContainerDOM.appendChild(newViewMoreDOM)
+
+      const newViewAllDOM = document.createElement('div')
+      newViewAllDOM.classList.add('view_all')
+      newViewAllDOM.innerHTML = `ver todos`
+      newButtonsContainerDOM.appendChild(newViewAllDOM)
+      
+      mainViewDOM.appendChild(newButtonsContainerDOM)
+
+      buttonsContainerDOM = newButtonsContainerDOM
+      
 
       newViewMoreDOM.onclick = () => {
         viewMoreMessages(result, _i)
       }
-
-      viewMoreDOM = newViewMoreDOM
-      mainViewDOM.appendChild(viewMoreDOM)
+ 
+      newViewAllDOM.onclick = () => {
+        newViewAllDOM.classList.add('loading')
+        setTimeout(() => {
+          viewAllMessages(result)
+        }, 0);
+      }
       break
     }
 
@@ -172,38 +190,35 @@ function populateMainView(result) {
 }
 
 function populateModal(result) {
+  modalDOM.innerHTML = ''
   // Messages
-  let messagesHTML = `<p>
-    <span>Total:</span>
-    <span>${result.messages.length}</span>
-    <span>100%</span>
-  </p>
-  <div class="bar">
-    <div class="amount"></div>
-  </div>`
+  const messagesHeaderDOM = document.createElement('h1')
+  messagesHeaderDOM.textContent = `mensajes`
+  modalDOM.appendChild(messagesHeaderDOM)
 
-  // Users
-  Object.keys(result.users).forEach(user => {
-    const percentage = (result.users[user].messages.length * 100 / result.messages.length).toFixed(2)
-    messagesHTML += `<p>
-    <span>${user}:</span>
-    <span>${result.users[user].messages.length}</span>
-    <span>${percentage}%</span>
-  </p>
-  <div class="bar">
-    <div class="amount" style="width: ${percentage}%;"></div>
-  </div>`
-  })
+  const messagesContainerDOM = document.createElement('div')
+  messagesContainerDOM.classList.add('messages')
+  messagesContainerDOM.innerHTML = getModalMessagesHTML(result)
+  modalDOM.appendChild(messagesContainerDOM)
+
+  const datesHeaderDOM = document.createElement('h1')
+  datesHeaderDOM.textContent = `fecha`
+  modalDOM.appendChild(datesHeaderDOM)
+
+  const datesContainerDOM = document.createElement('div')
+  datesContainerDOM.classList.add('dates')
+  datesContainerDOM.innerHTML = getModalDatesHTML(result)
+  modalDOM.appendChild(datesContainerDOM)
+
+  const activityHeaderDOM = document.createElement('h1')
+  activityHeaderDOM.textContent = `días mas activos`
+  modalDOM.appendChild(activityHeaderDOM)
 
   // Activity
-  const activityDOM = document.createElement('div')
-  activityDOM.classList.add('activity')
+  const activityContainerDOM = document.createElement('div')
+  activityContainerDOM.classList.add('activity')
 
-  const sortedDates = Object.entries(result.dates)
-  .sort(([,a],[,b]) => b-a)
-  .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
-
-  const arraySortedDates = Object.entries(sortedDates)
+  const arraySortedDates = getModalSortedDates(result)
   let i = 0;
   while (true) {
     const date = arraySortedDates[i];
@@ -243,66 +258,75 @@ function populateModal(result) {
     countDOM.innerText = date[1]
     activityBodyDOM.appendChild(countDOM)
 
-    activityDOM.appendChild(activityBodyDOM)
+    activityContainerDOM.appendChild(activityBodyDOM)
 
     i++
     if(i > arraySortedDates.length - 1) {
-      activityDOM.classList.remove('with_view_more')
+      activityContainerDOM.classList.remove('with_view_more')
       break
     }
 
     if(i > 4) {
-      const newViewMoreActivityDOM = document.createElement('div')
-      newViewMoreActivityDOM.classList.add('view_more')
-      newViewMoreActivityDOM.innerHTML = `ver más`
+      const newViewMoreActivity = document.createElement('div')
+      newViewMoreActivity.classList.add('view_more')
+      newViewMoreActivity.innerHTML = `ver más`
+      activityContainerDOM.appendChild(newViewMoreActivity)
 
-      newViewMoreActivityDOM.onclick = () => {
-        viewMoreActivity(result, arraySortedDates, i, activityDOM, newViewMoreActivityDOM)
+      const newViewAllActivityDOM = document.createElement('div')
+      newViewAllActivityDOM.classList.add('view_all')
+      newViewAllActivityDOM.innerHTML = `ver todos`
+      activityContainerDOM.appendChild(newViewAllActivityDOM)
+
+      newViewMoreActivity.onclick = () => {
+        viewMoreActivity(result, arraySortedDates, i, activityContainerDOM, [newViewAllActivityDOM, newViewMoreActivity])
       }
-      activityDOM.classList.add('with_view_more')
-      activityDOM.appendChild(newViewMoreActivityDOM)
+
+      newViewAllActivityDOM.onclick = () => {
+        viewAllActivity(result, arraySortedDates, i, activityContainerDOM, [newViewAllActivityDOM, newViewMoreActivity])
+      }
+      activityContainerDOM.classList.add('with_view_more')
       break
     }
   }
+  modalDOM.appendChild(activityContainerDOM)
 
-  modalDOM.innerHTML = `
-  <h1>Mensajes</h1>
-  <div class="messages">
-    ${messagesHTML}
-  </div>
-  <h1>Fecha</h1>
-  <div class="dates">
-    <p>
-      <span>Inicio:</span>
-      <span>${result.messages[0].date}</span>
-    </p>
-    <p>
-      <span>Final:</span>
-      <span>${result.messages[result.messages.length - 1].date}</span>
-    </p>
-  </div>
-  <h1>días mas activos</h1>
-  `
-
-  modalDOM.appendChild(activityDOM)
 }
 
 function viewMoreMessages(result, prevIndex) {
-  viewMoreDOM.remove()
+  buttonsContainerDOM.remove()
   while (true) {
-    if(_i > result.messages.length) break
-    
     _i++
+    if(_i > result.messages.length - 1) break
+    
     if(_i > (prevIndex + 50)) {
+      const newButtonsContainerDOM = document.createElement('div')
+      newButtonsContainerDOM.classList.add('buttons')
+
       const newViewMoreDOM = document.createElement('div')
       newViewMoreDOM.classList.add('view_more')
       newViewMoreDOM.innerHTML = `ver más`
+      newButtonsContainerDOM.appendChild(newViewMoreDOM)
+
+      const newViewAllDOM = document.createElement('div')
+      newViewAllDOM.classList.add('view_all')
+      newViewAllDOM.innerHTML = `ver todo`
+      newButtonsContainerDOM.appendChild(newViewAllDOM)
+      
+      mainViewDOM.appendChild(newButtonsContainerDOM)
+
+      buttonsContainerDOM = newButtonsContainerDOM
+      
 
       newViewMoreDOM.onclick = () => {
-        viewMoreMessages(result, (prevIndex+50))
+        viewMoreMessages(result, _i)
       }
-      viewMoreDOM = newViewMoreDOM
-      mainViewDOM.appendChild(newViewMoreDOM)
+ 
+      newViewAllDOM.onclick = () => {
+        newViewAllDOM.classList.add('loading')
+        setTimeout(() => {
+          viewAllMessages(result)
+        }, 0);
+      }
       break
     }
 
@@ -350,8 +374,59 @@ function viewMoreMessages(result, prevIndex) {
   }
 }
 
-function viewMoreActivity(result, arraySortedDates, prevIndex, activityDOM, viewMoreActivityDOM) {
-  viewMoreActivityDOM.remove()
+function viewAllMessages(result) {
+  buttonsContainerDOM.remove()
+  while (true) {
+    _i++
+    if(_i > result.messages.length - 1) break
+    
+    const message = result.messages[_i];
+    const messageDOM = document.createElement('div')
+    messageDOM.classList.add('message')
+    
+    if(result.users[message.user].number === 1) {
+      messageDOM.classList.add('rigth')
+    }
+    
+    // USER
+    const currentDate = message.date
+    if(result.users[message.user].number != prevUser || currentDate != prevDate) {
+      const userDOM = document.createElement('div')
+      userDOM.classList.add('user')
+      userDOM.innerHTML = message.user
+      prevUser = result.users[message.user].number
+      messageDOM.appendChild(userDOM)
+    }
+
+    // DATE
+    if(currentDate != prevDate) {
+      const dateDOM = document.createElement('div')
+      dateDOM.classList.add('date')
+      dateDOM.innerHTML = message.date
+      dateDOM.id = message.date
+      prevDate = currentDate
+      mainViewDOM.appendChild(dateDOM)
+    }
+
+    // TEXT
+    const textDOM = document.createElement('div')
+    textDOM.classList.add('text')
+    textDOM.innerText = message.message
+    messageDOM.appendChild(textDOM)
+    
+    // TIME
+    const timeDOM = document.createElement('div')
+    timeDOM.classList.add('time')
+    timeDOM.textContent = message.time
+    messageDOM.appendChild(timeDOM)
+  
+    mainViewDOM.appendChild(messageDOM)
+  }
+}
+
+
+function viewMoreActivity(result, arraySortedDates, prevIndex, activityContainerDOM, buttons) {
+  buttons.forEach(btn => btn.remove())
   // 9
   let i = prevIndex
   while (true) {
@@ -391,26 +466,34 @@ function viewMoreActivity(result, arraySortedDates, prevIndex, activityDOM, view
     countDOM.innerText = date[1]
     activityBodyDOM.appendChild(countDOM)
 
-    activityDOM.appendChild(activityBodyDOM)
+    activityContainerDOM.appendChild(activityBodyDOM)
 
     i++
     if(i > arraySortedDates.length - 1) {
-      activityDOM.classList.remove('with_view_more')
+      activityContainerDOM.classList.remove('with_view_more')
       break
     }
 
     if(i > (prevIndex + 4)) {
-      const newViewMoreActivityDOM = document.createElement('div')
-      newViewMoreActivityDOM.classList.add('view_more')
-      newViewMoreActivityDOM.innerHTML = `ver más`
+      const newViewMoreActivity = document.createElement('div')
+      newViewMoreActivity.classList.add('view_more')
+      newViewMoreActivity.innerHTML = `ver más`
+      activityContainerDOM.appendChild(newViewMoreActivity)
 
-      newViewMoreActivityDOM.onclick = () => {
-        viewMoreActivity(result, arraySortedDates, i, activityDOM, newViewMoreActivityDOM)
+      const newViewAllActivityDOM = document.createElement('div')
+      newViewAllActivityDOM.classList.add('view_all')
+      newViewAllActivityDOM.innerHTML = `ver todos`
+      activityContainerDOM.appendChild(newViewAllActivityDOM)
+
+      newViewMoreActivity.onclick = () => {
+        viewMoreActivity(result, arraySortedDates, i, activityContainerDOM, [newViewAllActivityDOM, newViewMoreActivity])
       }
-      activityDOM.appendChild(newViewMoreActivityDOM)
+
+      newViewAllActivityDOM.onclick = () => {
+        viewAllActivity(result, arraySortedDates, i, activityContainerDOM, [newViewAllActivityDOM, newViewMoreActivity])
+      }
+      activityContainerDOM.classList.add('with_view_more')
       break
-    }
-    if(prevIndex > (prevIndex + 10)) {
       break
     }
 
@@ -418,5 +501,101 @@ function viewMoreActivity(result, arraySortedDates, prevIndex, activityDOM, view
       break
     }
   }
+}
 
+function viewAllActivity(result, arraySortedDates, prevIndex, activityContainerDOM, buttons) {
+  buttons.forEach(btn => btn.remove())
+  // 9
+  let i = prevIndex
+  while (true) {
+    const date = arraySortedDates[i];
+    const percentage = ((date[1] * 100) / result.messages.length).toFixed(2)
+
+    const activityBodyDOM = document.createElement('div')
+
+    const dateDOM = document.createElement('p')
+    dateDOM.classList.add('date')
+    dateDOM.innerText = date[0]
+    dateDOM.onclick = () => {
+      dateDOM.classList.add('loading')
+      setTimeout(() => {
+        while (true) {
+          if(document.getElementById(date[0])) {
+            const dateFoundDOM = document.getElementById(date[0])
+            mainViewDOM.scrollTop = dateFoundDOM.offsetTop - dateFoundDOM.offsetHeight *2
+            modalDOM.classList.add('dnone')
+            dateDOM.classList.remove('loading')
+            break
+          }
+          viewMoreMessages(result, _i)
+        }
+      }, 0);
+    }
+    activityBodyDOM.appendChild(dateDOM)
+    
+    const barDOM = document.createElement('div')
+    barDOM.classList.add('bar')
+    barDOM.innerHTML = `<div class="percent">${percentage}%</div>
+    <div class="amount" style="height: ${percentage}%"></div>`
+    activityBodyDOM.appendChild(barDOM)
+
+    const countDOM = document.createElement('p')
+    countDOM.innerText = date[1]
+    activityBodyDOM.appendChild(countDOM)
+
+    activityContainerDOM.appendChild(activityBodyDOM)
+
+    i++
+    if(i > arraySortedDates.length - 1) {
+      activityContainerDOM.classList.remove('with_view_more')
+      break
+    }
+  }
+}
+
+function getModalMessagesHTML(result) {
+  let html = `
+  <p>
+    <span>Total:</span>
+    <span>${result.messages.length}</span>
+    <span>100%</span>
+  </p>
+  <div class="bar">
+    <div class="amount"></div>
+  </div>`
+
+  // Users
+  Object.keys(result.users).forEach(user => {
+    const percentage = (result.users[user].messages.length * 100 / result.messages.length).toFixed(2)
+    html += `<p>
+    <span>${user}:</span>
+    <span>${result.users[user].messages.length}</span>
+    <span>${percentage}%</span>
+  </p>
+  <div class="bar">
+    <div class="amount" style="width: ${percentage}%;"></div>
+  </div>`
+  })
+
+  return html
+}
+
+function getModalDatesHTML(result) {
+  return   `
+  <p>
+    <span>Inicio:</span>
+    <span>${result.messages[0].date}</span>
+  </p>
+  <p>
+    <span>Final:</span>
+    <span>${result.messages[result.messages.length - 1].date}</span>
+  </p>`
+}
+
+function getModalSortedDates(result) {
+  const sortedDates = Object.entries(result.dates)
+  .sort(([,a],[,b]) => b-a)
+  .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+  
+  return Object.entries(sortedDates)
 }
